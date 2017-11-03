4a18ca374860f443ceb2c3e5d44b3d2df9d36fef
KaungHtetLin <kaunghtetlin74.khl@gmail.com>
2017-11-03 15:07:46 +0630

    Finish Intent Setup

--- app/src/main/java/net/kaunghtetlin/padc_implicit_intnet/HomeActivity.java
+++ app/src/main/java/net/kaunghtetlin/padc_implicit_intnet/HomeActivity.java
@@ -1,15 +1,27 @@
 package net.kaunghtetlin.padc_implicit_intnet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class HomeActivity extends AppCompatActivity {
+
+    static final int PICK_IMAGE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,115 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btnShare=findViewById(R.id.btn_share);
+        Button btnMap=findViewById(R.id.btn_map);
+        Button btnPhoneCall=findViewById(R.id.btn_phone_call);
+        Button btnSendEmail=findViewById(R.id.btn_send_email);
+        Button btnTakePicture=findViewById(R.id.btn_take_picture);
+        Button btnSelectPicture=findViewById(R.id.btn_select_picture);
+        Button btnSaveEvent=findViewById(R.id.btn_save_event);
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+/*
+                String shareBody = "Kaung Htet Lin";
+
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));*/
+
+                String mimeType = "text/plain";
+                String title = "Example title";
+                String text="Kaung Htet Lin";
+
+                Intent shareIntent =   ShareCompat.IntentBuilder.from(HomeActivity.this)
+                        .setChooserTitle(title)
+                        .setType(mimeType)
+                        .setText(text)
+                        .getIntent();
+                if (shareIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(shareIntent);
+                }
+
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("google.navigation:q=Shwe Dagon Pagoda"));
+                startActivity(mapIntent);
+            }
+        });
+
+        btnPhoneCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0123456789"));
+                startActivity(intent);
+            }
+        });
+
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sendEmailIntent = new Intent(Intent.ACTION_SEND);
+                sendEmailIntent.setType("message/rfc822");
+                sendEmailIntent.putExtra(Intent.EXTRA_EMAIL  , new String[]{"kaunghtetlin@hotmail.com"});
+                sendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+                sendEmailIntent.putExtra(Intent.EXTRA_TEXT   , "body of email");
+                try {
+                    startActivity(Intent.createChooser(sendEmailIntent, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(HomeActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(takePictureIntent);
+            }
+        });
+
+        btnSelectPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        btnSaveEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEvent("PADC Class","Yangon",15,18);
+            }
+        });
+
+    }
+
+    public void addEvent(String title, String location, long begin, long end) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, title)
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     @Override
